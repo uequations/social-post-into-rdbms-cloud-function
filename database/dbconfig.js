15d8e698e2ff120096b46ca80c07eb0e5c4d130b
malkebu-lan <malkebu-lan@uequations.com>
2020-05-19 00:17:18 -0400

    additional logging

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -3,5 +3,6 @@ module.exports = {
     user: process.env.MYSQL_USER || "",
     password: process.env.MYSQL_PASSWORD || "",
     database: process.env.MYSQL_DATABASE || "uequations",
-    connectionLimit: process.env.MYSQL_CONNECTION_LIMIT || 4
+    connectionLimit: process.env.MYSQL_CONNECTION_LIMIT || 4,
+    acquireTimeout: process.env.MYSQL_ACQUIRE_TIMEOUT || 10000
 };
